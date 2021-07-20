68a2b3a5eebe988c810f469e2659d8775e3c3be8
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 13:15:55 +0200

    'Update'

--- DynamicFlow/scripts/FlowsDescription.lua
+++ DynamicFlow/scripts/FlowsDescription.lua
@@ -1,25 +1,3 @@
---[[----------------------------------------------------------------------------
-   
-  Application Name:
-  DynamicFlow                                                                                                               
-                                                                                              
-  Summary:
-  Creates and controls a Flow over the Flow API
-   
-  Description:
-  This sample is based on the ControlFlow sample as creates the same flow with it,
-  but creates and controls it using the Flow API
-  Introduction sample for creating and controling flows using the Flow API.
-  The sample first loads an existing flow out of a flow file.
-  In this flow, an output state is changed, depending on the state of two input ports. 
-  Via the API, a delay of 1s before setting the DigitalOut is added.  
-   
-  How to run:
-  The state of the Input ports can be toggled using the keyboard key "1" and "2" 
-  inside the Emulator tab.
-  See the console for the DigitalOutput state.
-      
-------------------------------------------------------------------------------]]
 
 --loading a flow from the flow-file  in the resources 
 handle = Flow.create()
